fc14066aa07440a69cf6e267c287ceec9f0834d6
Nanase Mizokoshi <1601022@st.asojuku.ac.jp>
2020-04-10 15:49:04 +0900

    fix: コードを書き換えました

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -1,53 +1,40 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import Info from '../public/svg/img/info.svg';
-import Fav from '../public/svg/img/fav.svg';
-import Del from '../public/svg/img/del.svg';
-import Add from '../public/svg/img/add.svg';
+import info from '../public/svg/img/info.svg';
+import fav from '../public/svg/img/fav.svg';
+import del from '../public/svg/img/del.svg';
+import add from '../public/svg/img/add.svg';
 
 const Icon = ({ name, checked, setter } = {}) => {
   const state = checked;
-  let icon;
-  switch (name) {
-    case 'info':
-      icon = <Info />;
-      break;
-    case 'fav':
-      icon = <Fav />;
-      break;
-    case 'del':
-      icon = <Del />;
-      break;
-    case 'add':
-      icon = <Add />;
-      break;
-  }
+  const iconList = { info, fav, del, add };
+
   const clickHandler = () => {
     setter(!state);
   };
-  return (
-    <Svg name={name} onClick={clickHandler} active={state}>
-      {icon}
-    </Svg>
-  );
+
+  const Svg = styled(iconList[name])`
+    ${SvgStyle}
+  `;
+  return <Svg name={name} onClick={clickHandler} active={state}></Svg>;
 };
 
 export default Icon;
 
-const Svg = styled.div`
+const SvgStyle = css`
   ${(props) => {
     let style;
     switch (props.name) {
       case 'info':
         style = css`
-          svg > path {
+          path {
             fill: ${(props) => (props.active ? props.theme.colors.blue : props.theme.colors.gray)};
           }
         `;
         break;
       case 'fav':
         style = css`
-          svg > path {
+          path {
             fill: ${(props) =>
               props.active ? props.theme.colors.yellow : props.theme.colors.gray};
           }
