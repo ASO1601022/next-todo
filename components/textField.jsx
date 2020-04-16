825e22811d820e08f6e2ef5da0531914f62e8ecd
Nanase Mizokoshi <1601022@st.asojuku.ac.jp>
2020-04-16 11:18:32 +0900

    fix: スタイルの調整、placeholderの追加

--- components/textField.jsx
+++ components/textField.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const TextField = ({ label, value = '', setter } = {}) => {
-  const changeHandler = e => {
+const TextField = ({ label, value = '', placeholder = '', setter } = {}) => {
+  const changeHandler = (e) => {
     setter && setter(e.target.value);
   };
   return (
     <div>
       {label && <LabelWrapper>{label}</LabelWrapper>}
-      <TextFieldWrapper value={value} onChange={changeHandler} />
+      <TextFieldWrapper value={value} onChange={changeHandler} placeholder={placeholder} />
     </div>
   );
 };
@@ -18,8 +18,14 @@ export default TextField;
 const TextFieldWrapper = styled.input`
   width: 100%;
   border-radius: 5px;
-  border: 1px solid ${props => props.theme.colors.borderline};
+  border: 1px solid ${(props) => props.theme.colors.borderline};
   box-sizing: border-box;
+  font-size: 18px;
+  padding: 15px 20px;
+  background-color: white;
+  ::placeholder {
+    color: ${(props) => props.theme.colors.gray};
+  }
 `;
 const LabelWrapper = styled.label`
   font-size: 14px;
